c2d048472c6dcb6e75dc513ed5e700c4f81d5a6c
Nick <nicholas.nocella@gmail.com>
2015-04-25 17:40:42 -0600

    Working Login - Jonny is the man!

--- app/src/main/java/com/roninsamakun/csbraintraining/LoginActivity.java
+++ app/src/main/java/com/roninsamakun/csbraintraining/LoginActivity.java
@@ -1,44 +1,49 @@
 package com.roninsamakun.csbraintraining;
 
-import android.app.AlertDialog;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
-import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Base64;
-import android.util.Log;
+import android.os.Bundle;
+import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import com.facebook.Session;
-import com.facebook.widget.LoginButton;
-// import needed libraries for event handling and gestures
 import android.widget.EditText;
 import android.widget.TextView;
-import android.view.MotionEvent;
-import android.view.GestureDetector;
-import android.support.v4.view.GestureDetectorCompat;
+import android.widget.Toast;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends ActionBarActivity implements
-GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener,
-View.OnClickListener {
+        GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener,
+        View.OnClickListener {
+
 
     private GestureDetectorCompat gestureDetector;
 
     Button buttonLogin, RegisterButton;
     EditText UserNameText, PasswordText;
-
-    UserLocalStore userLocalStore;
+    LoginDataBaseAdapter loginDataBaseAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // create a instance of SQLite Database
+        loginDataBaseAdapter=new LoginDataBaseAdapter(this);
+        loginDataBaseAdapter=loginDataBaseAdapter.open();
 
         UserNameText = (EditText) findViewById(R.id.UserNameText);
         PasswordText = (EditText) findViewById(R.id.PasswordText);
@@ -51,24 +56,33 @@ View.OnClickListener {
         this.gestureDetector = new GestureDetectorCompat(this, this);
         gestureDetector.setOnDoubleTapListener(this);
 
-    }
 
+    }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.buttonLogin:
-                String user_name = UserNameText.getText().toString();
-                String password = PasswordText.getText().toString();
+                // get The User name and Password
+                String userName=UserNameText.getText().toString();
+                String password=PasswordText.getText().toString();
 
-                User user = new User(user_name, password);
+                // fetch the Password form database for respective user name
+                String storedPassword=loginDataBaseAdapter.getSingleEntry(userName);
 
-                authenticate(user);
-
-                //userLocalStore.storeUserData(user);
-                //userLocalStore.setUserLoggedIn(true);
-
-                break;
+                // check if the Stored password matches with  Password entered by user
+                if(password.equals(storedPassword))
+                {
+                    startActivity(new Intent(LoginActivity.this, MainInterface.class));
+                    //Toast.makeText(LoginActivity.this, "Congrats: Login Successfull", Toast.LENGTH_LONG).show();
+                    //dialog.dismiss();
+                    break;
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(), "User Name or Password does not match", Toast.LENGTH_LONG).show();
+                    break;
+                }
 
             case R.id.RegisterButton:
                 startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
@@ -76,33 +90,16 @@ View.OnClickListener {
         }
     }
 
-    private void authenticate(User user) {
-        ServerRequests serverRequest = new ServerRequests(this);
-        serverRequest.fetchUserDataAsyncTask(user, new GetUserCallback() {
-            @Override
-            public void done(User returnedUser) {
-                if (returnedUser == null) {
-                    showErrorMessage();
-                } else {
-                    logUserIn(returnedUser);
-                }
-            }
-        });
-    }
 
-    private void showErrorMessage() {
-        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(LoginActivity.this);
-        dialogBuilder.setMessage("Incorrect user details");
-        dialogBuilder.setPositiveButton("Ok", null);
-        dialogBuilder.show();
-    }
 
-    private void logUserIn(User returnedUser) {
-        userLocalStore.storeUserData(returnedUser);
-        userLocalStore.setUserLoggedIn(true);
-        startActivity(new Intent(this, MainInterface.class));
-    }
 
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Close The Database
+        loginDataBaseAdapter.close();
+    }
 
     // methods for gestures
     @Override
@@ -157,26 +154,4 @@ View.OnClickListener {
     }
 
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
