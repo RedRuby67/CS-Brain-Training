c7544d1d071fbdbf504450c8cf4b0f060668c41c
roki7063 <roberto.kingsley@gmail.com>
2015-04-25 18:26:55 -0600

    Added questions

--- app/src/main/java/com/roninsamakun/csbraintraining/GameActivity.java
+++ app/src/main/java/com/roninsamakun/csbraintraining/GameActivity.java
@@ -48,18 +48,24 @@ GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
         Button answerD = (Button) findViewById(R.id.answerD);
 
         // Begin storing question data
-        String[] question1 = new String[]
-                { "A makefile has:", "a target", "dependencies", "commands", "all the above", "4", "unix" };
-        String[] question2 = new String[]
-                { "In C++, lines are terminated with a", "colon (:)", "semicolon (;)", "period (.)", "forward-slash (/)", "2", "cpp" };
-        String[] question3 = new String[]
-                { "What is the correct HTML tag for a line break?", "<lb>", "<bl>", "<br>", "<break>", "3", "html" };
-        String[] question4 = new String[]
-                { "How many times will 'hello' print from the following code?\n\tfor(i=2; i<=4; i++)\n\t\tprint 'hello';", "3", "2", "4", "None of the above", "1", "c" };
-        String[] questionX = new String[]
-                { "questionX", "answerA", "answerB", "answerC", "answerD", "1", "category" };
+		//{ "questionX", "answerA", "answerB", "answerC", "answerD", "1", "category" };
 
-        final String[][] QuestionsArray = new String[][] { question1, question2, question3, question4, questionX };
+        final String[][] QuestionsArray = new String[][] {
+			new String[] {"How many times will the word 'hello' appear? \n\n c = 5 \n a = 7 \n b = 8 \n c = b - c \n for i in range(0, c): \n    print('hello')", "5", "7", "8", "3", "4", "python"},
+			new String[] { "A ladybug has two methods: move(), which moves her one space in the direction she's facing, and rotate(), which rotates the direction she's facing 90 degrees clockwise. \n If the ladybug is on a board like the one below, what instruction would you need to use to get the ladybug from point A (where she sits looking North) to point B. \n . . . . . \n .A. .X.X. \n .X. . .X. \n .X.X. .B.", "rotate, move, rotate, move, rotate x3, move, rotate, move, rotatex3", "rotatex3, move, rotate, move, rotate x3, move, rotate, move, rotate", " move, rotate, move, rotate, move x3, rotate, move", "rotate, move x2, rotate, move, rotate, move", "1", "python" },
+			new String[] { "You have a program like the one below. What would be returned if you call F(3)? \n def F(n): \n     if n == 0: return 0 \n     elif n == 1: return 1 \n     else: return F(n-1)+F(n-2)", "2", "1", "3", "5", "1", "python" },
+			new String[] { "Which of these is used as an escape character in strings?", "/", "\\", "'", "''", "2", "general"},
+			new String[] { "What will c be equal to at the end of this program? \n a = 3 \n b = 2 \n c = a + b \n b = 4 \n a = a + b \n c = c + a", "12", "5", "10", "7", "1", "python" },
+			new String[] { "What will c be equal to at the end of this program? \n a = 4 \n b = 5 \n c = a \n d = c \n if (a + 2 > b + 1): \n     c = 2 * c \n else: \n     c = c / 2", "2", "4", "5", "3", "1", "python" },
+			new String[] { "What will print? \n a = 'foo' \n b = 'bar' \n c = ' ' \n d = a + b + c + a + b \n print('d')", "d", "foobar foobar", "foobarfoobar", "foobar", "1", "python" },
+			new String[] { "What will print? \n a = 'foo' \n b = 'bar' \n c = ' ' \n d = a + b + c + a + b \n print(d)", "d", "foobar foobar", "foobarfoobar", "foobar", "2", "python" },
+			new String[] { "What will print? \n\n a = 5 \n b = 5 \n\n if (a - b < 0): \n    print('foo') \n elif (5 == 6): \n     print('baf') \n elif (7 == 8): \n     print('bof') \n else: \n     print('bar')", "foo", "bar", "baf", "bof", "2", "python" },
+			new String[] { "What will the following code print? \n\n mystring = 'Hello' \n mystring += ' world.' \n print mystring", "Hello World", "Helloworld", "Hello", " world", "1", "python" }
+			new String[] { "A makefile has:", "a target", "dependencies", "commands", "all the above", "4", "unix" }
+			new String[] { "In C++, lines are terminated with a", "colon (:)", "semicolon (;)", "period (.)", "forward-slash (/)", "2", "cpp" }
+			new String[] { "What is the correct HTML tag for a line break?", "<lb>", "<bl>", "<br>", "<break>", "3", "html" }
+			new String[] { "How many times will 'hello' print from the following code?\n\tfor(i=2; i<=4; i++)\n\t\tprint 'hello';", "3", "2", "4", "None of the above", "1", "c" }
+			};
 
         int totalQuestions = QuestionsArray.length;
         // Randomly generate a number in [0,totalQuestions) to determine which question loads
